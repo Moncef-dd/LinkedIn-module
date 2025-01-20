87c39d17d96ba4c1b32f339ee788c9b6e7692844
Moncef SAMAI <Moncef.samai.rol2@gmail.com>
2025-01-20 15:01:02 -0600

    Update test.py to a module format

--- test.py
+++ test.py
@@ -4,91 +4,100 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
 
-ACCOUNT_EMAIL = YOUR LOGIN EMAIL
-ACCOUNT_PASSWORD = YOUR LOGIN PASSWORD
-PHONE = YOUR PHONE NUMBER
+class LinkedInModule: 
+    def __init__(self, email, password, phoneNumber): 
+        self.email = email 
+        self.password = password 
+        self.phoneNumber = phoneNumber
 
-
-def abort_application():
+    def abort_application(self):
     
-    close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
-    close_button.click()
+     close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+     close_button.click()
 
-    time.sleep(2)
-    # Click Discard Button
-    discard_button = driver.find_elements(by=By.CLASS_NAME, value="artdeco-modal__confirm-dialog-btn")[1]
-    discard_button.click()
+     time.sleep(2)
+     # Click Discard Button
+     discard_button = driver.find_elements(by=By.CLASS_NAME, value="artdeco-modal__confirm-dialog-btn")[1]
+     discard_button.click()
 
 
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+     chrome_options = webdriver.ChromeOptions()
+     chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
+     driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3586148395&f_LF=f_AL&geoId=101356765&"
+     driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3586148395&f_LF=f_AL&geoId=101356765&"
            "keywords=python&location=London%2C%20England%2C%20United%20Kingdom&refresh=true")
 
-time.sleep(2)
-reject_button = driver.find_element(by=By.CSS_SELECTOR, value='button[action-type="DENY"]')
-reject_button.click()
+     time.sleep(2)
+     reject_button = driver.find_element(by=By.CSS_SELECTOR, value='button[action-type="DENY"]')
+     reject_button.click()
 
 
-time.sleep(2)
-sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
-sign_in_button.click()
+     time.sleep(2)
+     sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
+     sign_in_button.click()
 
-time.sleep(5)
-email_field = driver.find_element(by=By.ID, value="username")
-email_field.send_keys(ACCOUNT_EMAIL)
-password_field = driver.find_element(by=By.ID, value="password")
-password_field.send_keys(ACCOUNT_PASSWORD)
-password_field.send_keys(Keys.ENTER)
+     time.sleep(5)
+     email_field = driver.find_element(by=By.ID, value="username")
+     email_field.send_keys(self.email)
+     password_field = driver.find_element(by=By.ID, value="password")
+     password_field.send_keys(self.password)
+     password_field.send_keys(Keys.ENTER)
 
-# CAPTCHA - Solve Puzzle Manually
-input("Press Enter when you have solved the Captcha")
-
-
-time.sleep(5)
-all_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
+     # CAPTCHA - Solve Puzzle Manually
+     input("Press Enter when you have solved the Captcha")
 
 
-for listing in all_listings:
-    print("Opening Listing")
-    listing.click()
-    time.sleep(2)
-    try:
-
-        apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
-        apply_button.click()
+     time.sleep(5)
+     all_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
 
 
+     for listing in all_listings:
+         print("Opening Listing")
+         listing.click()
+         time.sleep(2)
+         try:
 
-        time.sleep(5)
-        phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
-        if phone.text == "":
-            phone.send_keys(PHONE)
+             apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+             apply_button.click()
+  
 
 
-        submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
-        if submit_button.get_attribute("data-control-name") == "continue_unify":
-            abort_application()
-            print("Complex application, skipped.")
-            continue
-        else:
+             time.sleep(5)
+             phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
+             if phone.text == "":
+                 phone.send_keys(self.phoneNumber)
 
-            print("Submitting job application")
-            submit_button.click()
 
-        time.sleep(2)
-        # Click Close Button
-        close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
-        close_button.click()
+             submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+             if submit_button.get_attribute("data-control-name") == "continue_unify":
+                 abort_application()
+                 print("Complex application, skipped.")
+                 continue
+             else:
 
-    except NoSuchElementException:
-        abort_application()
-        print("No application button, skipped.")
-        continue
+                 print("Submitting job application")
+                 submit_button.click()
 
-time.sleep(5)
-driver.quit()
+             time.sleep(2)
+             # Click Close Button
+             close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+             close_button.click()
+
+        except NoSuchElementException:
+             abort_application()
+             print("No application button, skipped.")
+             continue
+
+     time.sleep(5)
+     driver.quit()
+
+def main(): 
+    applyer = LinkedInModule(email, pw, number)
+    applyer.abort_application()
+
+
+if __name__ == "__main__": 
+    main()
